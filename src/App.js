576be939942151f462bacfe88de09788a44a79c1
junaid-mohamed <64761352+Junaid-Mohamed@users.noreply.github.com>
2023-10-14 10:54:51 +0000

    feat : highlight of button selection

--- src/App.js
+++ src/App.js
@@ -3,25 +3,42 @@ import "./styles.css";
 import { useState } from "react";
 
 export default function App() {
+  const [selectedTopicButton, setTopicButton] = useState(0);
+  const [selectedGenreButton, setGenreButton] = useState(0);
   const [selectedtopic, setTopic] = useState("Books");
   const [selectedGenre, setGenre] = useState("Javascript");
   // const [buttonColor, setColor] = useState("");
 
-  function handleTopicClicked(topic) {
-    // console.log(Object.keys(recomendationCollection[topic]));
+  // function handleSelectedButton(index) {
+  //   setTopicButton(index);
+  // }
+
+  // console.log(selectedTopicButton);
+
+  function handleTopicClicked(topic, index) {
+    setTopicButton(index);
+
     setTopic(topic);
     if (topic === "Books") {
       setGenre("Javascript");
+      setGenreButton(0);
     } else if (topic === "Travel") {
       setGenre("Ecotourism");
+      setGenreButton(2);
     } else {
-      setGenre("food1");
+      setGenre("Indian");
+      setGenreButton(0);
     }
     // console.log(recomendationCollection[topic]);
     // setGenre(recomendationCollection[topic][0]);
   }
 
-  function handleGenreClicked(genre) {
+  function handleClick(event) {
+    console.log(event.target);
+  }
+
+  function handleGenreClicked(genre, index) {
+    setGenreButton(index);
     setGenre(genre);
   }
 
@@ -31,26 +48,39 @@ export default function App() {
       <p>
         <small>Select a topic to be recomended</small>
       </p>
-      {Object.keys(recomendationCollection).map((topic) => (
+      {Object.keys(recomendationCollection).map((topic, index) => (
         <button
           // style={{ backgroundColor: buttonColor }}
           className="topicButton"
+          style={{
+            backgroundColor:
+              selectedTopicButton === index ? "beige" : "aquamarine",
+          }}
           key={topic}
-          onClick={() => handleTopicClicked(topic)}
+          id={topic}
+          onClick={() => {
+            handleTopicClicked(topic, index);
+          }}
         >
           {topic}
         </button>
       ))}
       <hr />
-      {Object.keys(recomendationCollection[selectedtopic]).map((genre) => (
-        <button
-          className="genreButton"
-          key={genre}
-          onClick={() => handleGenreClicked(genre)}
-        >
-          {genre}
-        </button>
-      ))}
+      {Object.keys(recomendationCollection[selectedtopic]).map(
+        (genre, index) => (
+          <button
+            className="genreButton"
+            style={{
+              backgroundColor:
+                selectedGenreButton === index ? "beige" : "aquamarine",
+            }}
+            key={genre}
+            onClick={() => handleGenreClicked(genre, index)}
+          >
+            {genre}
+          </button>
+        ),
+      )}
       <hr />
       {
         <div style={{ textAlign: "left" }}>
@@ -64,7 +94,7 @@ export default function App() {
                   </div>
                   <div style={{ fontSize: "small" }}>{item.ratings}</div>
                 </li>
-              )
+              ),
             )}
           </ul>
         </div>
--- src/recomendationCollection.js
+++ src/recomendationCollection.js
@@ -3,148 +3,174 @@ export const recomendationCollection = {
     Javascript: [
       {
         name: "Eloquent JavaScript",
-        ratings: "4/5"
+        ratings: "4/5",
       },
       {
         name: "You Don't Know JS",
-        ratings: "3.5/5"
+        ratings: "3.5/5",
       },
       {
         name: "A Smarter Way to Learn JavaScript",
-        ratings: "3/5"
-      }
+        ratings: "3/5",
+      },
     ],
     Fiction: [
       {
         name: "Harry Potter and the Sorcerer's Stone",
-        ratings: "4.5/5"
+        ratings: "4.5/5",
       },
       {
         name: "It Ends with Us",
-        ratings: "5/5"
+        ratings: "5/5",
       },
       {
         name: "I Too Had a Love Story",
-        ratings: "4/5"
-      }
+        ratings: "4/5",
+      },
     ],
     Business: [
       {
         name: "Never Split the Difference",
-        ratings: "3.5/5"
+        ratings: "3.5/5",
       },
       {
         name: "Loonshots",
-        ratings: "5/5"
+        ratings: "5/5",
       },
       {
         name: "The Almanack Of Naval Ravikant",
-        ratings: "5/5"
-      }
+        ratings: "5/5",
+      },
     ],
     "Self Help": [
       {
         name: "How to win friends and influence people",
-        ratings: "4.5/5"
+        ratings: "4.5/5",
       },
       {
         name: "The 7 Habits of Highly Effective People",
-        ratings: "4/5"
+        ratings: "4/5",
       },
       {
         name: "Atomic Habits",
-        ratings: "5/5"
-      }
-    ]
+        ratings: "5/5",
+      },
+    ],
   },
   Travel: {
     "Scuba diving destinations": [
       {
         name: "Havelock Island, Andamans",
-        ratings: "4/5"
+        ratings: "4/5",
       },
       {
         name: "Grand Island, Goa",
-        ratings: "3.5/5"
+        ratings: "3.5/5",
       },
       {
         name: "Kovalam, Kerala",
-        ratings: "4/5"
-      }
+        ratings: "4/5",
+      },
     ],
     "Adventure & hiking": [
       {
-        name: "Harry Potter and the Sorcerer's Stone",
-        ratings: "4.5/5"
+        name: "The Grand Indrahar Pass, Himachal Pradesh",
+        ratings: "4.5/5",
       },
       {
-        name: "It Ends with Us",
-        ratings: "5/5"
+        name: "Hampta Pass Trek, Manali",
+        ratings: "5/5",
       },
       {
-        name: "It Ends with Us",
-        ratings: "5/5"
-      }
+        name: "Kuari Pass Trek, Uttarakhand",
+        ratings: "5/5",
+      },
     ],
     Ecotourism: [
       {
-        name: "Never Split the Difference",
-        ratings: "3.5/5"
+        name: "Honey Hills – Thenmala Eco-Tourism, Kerala",
+        ratings: "4/5",
       },
       {
-        name: "Loonshots",
-        ratings: "5/5"
+        name: "Bandipur National Park, Karnataka",
+        ratings: "5/5",
       },
       {
-        name: "It Ends with Us",
-        ratings: "5/5"
-      }
+        name: "Kaziranga National Park, Assam",
+        ratings: "5/5",
+      },
     ],
     "Religious destinations": [
       {
-        name: "Never Split the Difference",
-        ratings: "3.5/5"
+        name: "Ajmer Sharif Dargah, Ajmer",
+        ratings: "4.5/5",
       },
       {
-        name: "Loonshots",
-        ratings: "5/5"
+        name: "Varanasi, Uttar Pradesh ",
+        ratings: "5/5",
       },
       {
-        name: "It Ends with Us",
-        ratings: "5/5"
-      }
-    ]
+        name: "Velankanni Church, Tamil Nadu",
+        ratings: "5/5",
+      },
+    ],
   },
   Food: {
-    food1: [
+    Indian: [
       {
-        name: "Eloquent JavaScript",
-        ratings: "4/5"
+        name: "Biryani",
+        ratings: "5/5",
       },
       {
-        name: "You Don't Know JS",
-        ratings: "3.5/5"
-      }
+        name: "Butter chicken",
+        ratings: "4.5/5",
+      },
+      {
+        name: "Tandoori meat",
+        ratings: "3.5/5",
+      },
     ],
-    food2: [
+    Chinese: [
       {
-        name: "Harry Potter and the Sorcerer's Stone",
-        ratings: "4.5/5"
+        name: "Spring Rolls",
+        ratings: "4.5/5",
       },
       {
-        name: "It Ends with Us",
-        ratings: "5/5"
-      }
+        name: "Chow Mein",
+        ratings: "5/5",
+      },
+      {
+        name: "Kung Pao Chicken",
+        ratings: "5/5",
+      },
     ],
-    food3: [
+    Italian: [
       {
-        name: "Never Split the Difference",
-        ratings: "3.5/5"
+        name: "Pizza",
+        ratings: "3.5/5",
       },
       {
-        name: "Loonshots",
-        ratings: "5/5"
-      }
-    ]
-  }
+        name: "Pasta",
+        ratings: "5/5",
+      },
+      {
+        name: "Vitello Tonnato",
+        ratings: "5/5",
+      },
+    ],
+    American: [
+      {
+        name: "barbecue",
+        ratings: "3.5/5",
+      },
+      {
+        name: "hot dogs",
+        ratings: "5/5",
+      },
+      {
+        name: "Chocolate chip cookie",
+        ratings: "5/5",
+      },
+    ],
+  },
 };
